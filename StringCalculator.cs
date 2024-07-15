7f5301bce730e1b72be7e3e8da2b99ebb0324da4
gaztu <gaztu@hotmail.com>
2024-07-16 09:07:34 +1200

    Step 7: Refactor delimiter split into separate function

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -6,20 +6,8 @@ public class StringCalculator
     {
         if (String.IsNullOrEmpty(numbers)) return 0;
 
-        string[] delimiters = new string[] {",", "\n"};
-        if (numbers.Contains("//"))
-        {
-            numbers = numbers.TrimStart('/');
-            if (numbers.Contains('['))
-            {
-                var customDelimiter = numbers.Substring(1, numbers.IndexOf(']') - 1);
-                delimiters = delimiters.Concat(new string[] { customDelimiter }).ToArray();
-                numbers = numbers.Substring(customDelimiter.Length + 3);
-            } else {
-                delimiters = delimiters.Concat(new string[] { numbers[0].ToString() }).ToArray();
-                numbers = numbers[2..];
-            }
-        }
+        string[] delimiters = GetDelimiters(numbers);
+        if (numbers.Contains("//")) numbers = numbers[(numbers.IndexOf('\n') + 1)..];
 
         var numArray = numbers.Split(delimiters, StringSplitOptions.None).Select(int.Parse).Where(x => x <= 1000);
         var negativeNumbers = numArray.Where(x => x < 0);
@@ -33,6 +21,25 @@ public class StringCalculator
             throw new Exception(message);
         }
 
-        return numArray.Sum();;
+        return numArray.Sum();
+    }
+
+    private string[] GetDelimiters(string numbers){
+        string[] result = new string[] {",", "\n"};
+        if (numbers.Contains("//"))
+        {
+            if (numbers.Contains('['))
+            {
+                var customDelimiter = numbers[3..numbers.IndexOf(']')];
+                result = result.Concat(new string[] { customDelimiter }).ToArray();
+            } else {
+                result = result.Concat(new string[] { numbers[2].ToString() }).ToArray();
+            }
+        }
+        return result;
+    }
+
+    private void CheckNegatives(){
+
     }
 }
