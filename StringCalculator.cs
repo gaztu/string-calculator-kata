d14d83c3cd436233998d8d7b1f5395d275932c5c
gaztu <gaztu@hotmail.com>
2024-07-16 09:16:41 +1200

    Step 7: Refactor checking for negative numbers

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -9,37 +9,44 @@ public class StringCalculator
         string[] delimiters = GetDelimiters(numbers);
         if (numbers.Contains("//")) numbers = numbers[(numbers.IndexOf('\n') + 1)..];
 
-        var numArray = numbers.Split(delimiters, StringSplitOptions.None).Select(int.Parse).Where(x => x <= 1000);
-        var negativeNumbers = numArray.Where(x => x < 0);
-        if (negativeNumbers.Any()) {
-            string message = "Negatives not allowed: ";
-            foreach(int num in negativeNumbers)
-            {
-                message += num.ToString() + ",";
-            }
-            message = message.TrimEnd(',');
-            throw new Exception(message);
-        }
+        var numList = numbers.Split(delimiters, StringSplitOptions.None)
+            .Select(int.Parse)
+            .Where(x => x <= 1000);
+        CheckNegatives(numList);
 
-        return numArray.Sum();
+        return numList.Sum();
     }
 
-    private string[] GetDelimiters(string numbers){
-        string[] result = new string[] {",", "\n"};
+    private string[] GetDelimiters(string numbers)
+    {
+        string[] result = new string[] { ",", "\n" };
         if (numbers.Contains("//"))
         {
             if (numbers.Contains('['))
             {
                 var customDelimiter = numbers[3..numbers.IndexOf(']')];
                 result = result.Concat(new string[] { customDelimiter }).ToArray();
-            } else {
+            }
+            else
+            {
                 result = result.Concat(new string[] { numbers[2].ToString() }).ToArray();
             }
         }
         return result;
     }
 
-    private void CheckNegatives(){
-
+    private void CheckNegatives(IEnumerable<int> numArray)
+    {
+        var negativeNumbers = numArray.Where(x => x < 0);
+        if (negativeNumbers.Any())
+        {
+            string message = "Negatives not allowed: ";
+            foreach (int num in negativeNumbers)
+            {
+                message += num.ToString() + ",";
+            }
+            message = message.TrimEnd(',');
+            throw new Exception(message);
+        }
     }
 }
