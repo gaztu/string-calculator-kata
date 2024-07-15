ccc49460731bedd9bb710433f58a8be67947c9e8
gaztu <gaztu@hotmail.com>
2024-07-15 12:56:05 +1200

    Step 1 Test 1

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -2,5 +2,8 @@ namespace string_calculator;
 
 public class StringCalculator
 {
-    
+    public int Add(string numbers)
+    {
+        return 0;
+    }
 }
--- StringCalculator_Add.cs
+++ StringCalculator_Add.cs
@@ -3,8 +3,15 @@ namespace string_calculator;
 public class StringCalculator_Add
 {
     [Fact]
-    public void Test1()
+    public void Add_EmptyString_ReturnsZero()
     {
-        
+        // Arrange
+        StringCalculator calc = new();
+
+        // Act
+        var result = calc.Add("");
+
+        // Assert
+        Assert.Equal(0, result);
     }
 }
